08125dcb5eeef3d22c836c81503dde751bcba2d8
Manshu Sengar <89942713+ManshuSengar@users.noreply.github.com>
2025-08-28 19:32:11 +0530

    Time: 36 ms (81.84%), Space: 53.4 MB (46.74%) - LeetHub

---
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.js
@@ -0,0 +1,24 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function(n) {
+    let l=1;
+    let r=n;
+    while(l<=r){
+        let m=Math.floor(l+(r-l)/2);
+        let pick= guess(m);
+        if(pick==0) return m;
+        else if(pick==1) l=m+1;
+        else r=m-1;
+    }
+};
