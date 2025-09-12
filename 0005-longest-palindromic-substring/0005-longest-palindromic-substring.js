debce890d1e6649fff9fd8d336788dce644f86d6
Manshu Sengar <89942713+ManshuSengar@users.noreply.github.com>
2025-09-12 22:34:35 +0530

    Time: 19 ms (78.64%), Space: 57.8 MB (48.13%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,27 @@
+function longestPalindrome(s) {
+    if (s.length < 1) return "";
+
+    let start = 0;
+    let end = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const len1 = expandAroundCenter(s, i, i);      // Odd length
+        const len2 = expandAroundCenter(s, i, i + 1);  // Even length
+        const maxLen = Math.max(len1, len2);
+
+        if (maxLen > end - start) {
+            start = i - Math.floor((maxLen - 1) / 2);
+            end = i + Math.floor(maxLen / 2);
+        }
+    }
+
+    return s.substring(start, end + 1);
+}
+
+function expandAroundCenter(s, left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+        left--;
+        right++;
+    }
+    return right - left - 1; // Length of the palindrome
+}
