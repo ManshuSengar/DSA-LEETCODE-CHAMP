f6df3735bd52d679a04425bbb3910f923d215880
Manshu Sengar <89942713+ManshuSengar@users.noreply.github.com>
2025-09-14 00:56:49 +0530

    Time: 0 ms (100%), Space: 53.1 MB (82.57%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let count=0;
+    let i=s.length-1;
+    while(i>=0){
+        if(count!=0 && s[i]==" " ) return count;
+        else if(s[i]!=" ") count++;
+        i--;
+    }
+    return count;
+};
