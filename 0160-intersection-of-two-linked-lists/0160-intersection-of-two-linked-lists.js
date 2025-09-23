ba08012816b00b7b6a43ef3493a4a106d5dc7618
Manshu Sengar <89942713+ManshuSengar@users.noreply.github.com>
2025-09-23 20:33:32 +0530

    Time: 50 ms (89.52%), Space: 62.1 MB (95.08%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -1,10 +1,58 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
+// /**
+ 
+//  /**
+//  * Definition for singly-linked list.
+//  * function ListNode(val) {
+//  *     this.val = val;
+//  *     this.next = null;
+//  * }
+//  */
+
+// /**
+//  * @param {ListNode} headA
+//  * @param {ListNode} headB
+//  * @return {ListNode}
+//  */
+// var getIntersectionNode = function(headA, headB) {
+//     let sizeA=0;
+//     let sizeB=0;
+//     let currA=headA;
+//     let currB=headB;
+//     while(currA){
+//         currA=currA.next;
+//         sizeA++;
+//     }
+//     while(currB){
+//         currB=currB.next;
+//         sizeB++;
+//     }
+//     let diff= Math.abs(sizeA - sizeB);
+//     currB=headB;
+//     currA=headA;
+//     if(sizeA>sizeB){
+//         for(let i=0;i<diff;i++){
+//             currA=currA.next;
+//         }
+//     }
+//     else{
+//          for(let i=0;i<diff;i++){
+//             currB=currB.next;
+//         }
+//     }
+//      while(currA){
+//         if(currA===currB) {
+//             return currA;
+//         }
+//         currA=currA.next;
+//         currB=currB.next;
+//     }
+
+//     return null;
+// };
+ 
+ 
+ 
+ 
 
 /**
  * @param {ListNode} headA
@@ -12,38 +60,11 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    let sizeA=0;
-    let sizeB=0;
     let currA=headA;
     let currB=headB;
-    while(currA){
-        currA=currA.next;
-        sizeA++;
+    while(currA!=currB){
+        currA=currA===null ? headB:currA.next;
+        currB=currB===null ? headA:currB.next;
     }
-    while(currB){
-        currB=currB.next;
-        sizeB++;
-    }
-    let diff= Math.abs(sizeA - sizeB);
-    currB=headB;
-    currA=headA;
-    if(sizeA>sizeB){
-        for(let i=0;i<diff;i++){
-            currA=currA.next;
-        }
-    }
-    else{
-         for(let i=0;i<diff;i++){
-            currB=currB.next;
-        }
-    }
-     while(currA){
-        if(currA===currB) {
-            return currA;
-        }
-        currA=currA.next;
-        currB=currB.next;
-    }
-
-    return null;
+    return currA;
 };
