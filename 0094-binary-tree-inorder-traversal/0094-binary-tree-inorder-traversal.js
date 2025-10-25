12b3408ca78435a1cecb9a076d134fbeb2fc3e94
Manshu Sengar <89942713+ManshuSengar@users.noreply.github.com>
2025-10-26 00:34:35 +0530

    Time: 0 ms (100%), Space: 54.6 MB (22.19%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -11,13 +11,30 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
-    let arr=[];
-    function traversal(curr){
-      if(!curr) return;
-      traversal(curr.left);
+   if(!root) return [];
+   let arr=[];
+   let stack=[];
+   let curr=root;
+   while(curr || stack.length){
+      while(curr){
+        stack.push(curr);
+        curr=curr.left;
+      }
+      curr=stack.pop();
       arr.push(curr.val);
-      traversal(curr.right);
-    }
-    traversal(root);
-    return arr;
+      curr= curr?.right;
+   }
+   return arr;
 };
+
+// var inorderTraversal = function(root) {
+//     let arr=[];
+//     function traversal(curr){
+//       if(!curr) return;
+//       traversal(curr.left);
+//       arr.push(curr.val);
+//       traversal(curr.right);
+//     }
+//     traversal(root);
+//     return arr;
+// };
