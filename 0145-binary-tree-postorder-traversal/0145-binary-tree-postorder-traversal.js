c8a7cb851fdc152a92ff17ca45703c9d5e590554
Manshu Sengar <89942713+ManshuSengar@users.noreply.github.com>
2025-10-27 19:41:19 +0530

    Time: 0 ms (100%), Space: 54.6 MB (24.08%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
@@ -10,14 +10,34 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+// var postorderTraversal = function(root) {
+//     let arr=[];
+//     function traversal(curr){
+//         if(!curr) return;
+//         traversal(curr.left);
+//         traversal(curr.right);
+//         arr.push(curr.val);
+//     }
+//     traversal(root);
+//     return arr;
+// };
+
+
 var postorderTraversal = function(root) {
-    let arr=[];
-    function traversal(curr){
-        if(!curr) return;
-        traversal(curr.left);
-        traversal(curr.right);
-        arr.push(curr.val);
+    if(!root) return [];
+    let s1=[root];
+    let s2=[];
+    while(s1.length){
+        let curr=s1.pop();
+        s2.push(curr);
+        curr.left && s1.push(curr.left);
+        curr.right && s1.push(curr.right);
     }
-    traversal(root);
-    return arr;
+   let ans=[];
+    while(s2.length){
+        let curr=s2.pop();
+        ans.push(curr.val);
+    }
+    return ans;
 };
+
