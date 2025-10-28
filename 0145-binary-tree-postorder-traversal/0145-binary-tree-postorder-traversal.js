31db900f920c267e342fb38636986bd800f3c163
Manshu Sengar <89942713+ManshuSengar@users.noreply.github.com>
2025-10-28 19:05:32 +0530

    Time: 0 ms (100%), Space: 55.8 MB (6.35%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
@@ -10,6 +10,40 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+var postorderTraversal = function(root) {
+    let stack=[];
+    let curr=root;
+    let ans=[];
+    let isVisted=null;
+    while(stack.length || curr){
+        while(curr){
+            stack.push(curr);
+            curr=curr.left;
+        }
+        let peek=stack[stack.length-1];
+        if(peek.right && isVisted!=peek.right){
+            curr=peek.right;
+        }else{
+           ans.push(peek.val);
+           isVisted=stack.pop();
+        }
+    }
+
+    return ans;
+
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * 
+ */
 // var postorderTraversal = function(root) {
 //     let arr=[];
 //     function traversal(curr){
@@ -23,21 +57,21 @@
 // };
 
 
-var postorderTraversal = function(root) {
-    if(!root) return [];
-    let s1=[root];
-    let s2=[];
-    while(s1.length){
-        let curr=s1.pop();
-        s2.push(curr);
-        curr.left && s1.push(curr.left);
-        curr.right && s1.push(curr.right);
-    }
-   let ans=[];
-    while(s2.length){
-        let curr=s2.pop();
-        ans.push(curr.val);
-    }
-    return ans;
-};
+// var postorderTraversal = function(root) {
+//     if(!root) return [];
+//     let s1=[root];
+//     let s2=[];
+//     while(s1.length){
+//         let curr=s1.pop();
+//         s2.push(curr);
+//         curr.left && s1.push(curr.left);
+//         curr.right && s1.push(curr.right);
+//     }
+//    let ans=[];
+//     while(s2.length){
+//         let curr=s2.pop();
+//         ans.push(curr.val);
+//     }
+//     return ans;
+// };
 
